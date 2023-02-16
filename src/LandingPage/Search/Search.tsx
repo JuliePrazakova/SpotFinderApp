0adf42f417de6d540c26706c811d407592962e95
Julie Pražáková <xprazak6@mendelu.cz>
2023-02-16 10:48:36 +0100

    Deleted TourPage, search function

--- src/LandingPage/Search/Search.tsx
+++ src/LandingPage/Search/Search.tsx
@@ -1,27 +1,46 @@
 // Types
 // Styles
 import { Wrapper, Buttons } from './Search.styles'
-import * as React from 'react'
+import React, { useState } from 'react';
+
 
 
 
 export type ISearchProps = Record<string, unknown>
 
 const Search: React.FunctionComponent<ISearchProps> = () => {
+  const [hidden, setHidden] = useState(true);
+
+  function changeSearchbar(){
+    if(hidden == true){
+      setHidden(false);
+    }else {
+      setHidden(true);
+    }
+  }
 
     return (
         <div>
             <Buttons>
-                <div className='type-button active'>One place</div>
-                <div className='type-button'>Road trip</div>
+                <div className='type-button active'><button onClick={changeSearchbar}>One place</button></div>
+                <div className='type-button'><button onClick={changeSearchbar}>Road trip</button></div>
             </Buttons>
         <Wrapper>
-          <div className='label left' >Where
+          {hidden && <OneWayBar />}
+          {!hidden && <RoadTripBar />}
+          
+        </Wrapper>
+        </div>
+    )
+}
+const OneWayBar = () => (
+  <div>
+  <div className='label left' >Where
             <input type="text" placeholder="Location" className="search-left" />
           </div>
             <div className="vl"></div>
 
-            <div className='label' >From
+            <div className='label' >Where
               <select className="dropdown">
                 <option value="date">Mo 12.2. </option>
                 <option value="date">Tu 13.2.</option>
@@ -31,29 +50,45 @@ const Search: React.FunctionComponent<ISearchProps> = () => {
             </div>
             <div className="vl"></div>
 
-            <div className='label' >To
-            <select className="dropdown">
-              <option value="date">Thr 15.2. </option>
-              <option value="date">Tu 13.2.</option>
-              <option value="date">We 14.2.</option>
-              <option value="date">Thr 15.2. </option>
-            </select>
-          </div>
-            <div className="vl"></div>
-
-            <div className='label' >Who
-                <select className="dropdown">
-                    <option value="0">Add guests </option>
-                    <option value="1">1 </option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4 </option>
-                </select>
+            <div className='label' >Radius
+              <input type="text" placeholder="Radius" className="search-left" />
             </div>
               <div className="button">SEARCH</div>
-        </Wrapper>
-        </div>
+              </div>
+  )
+
+  const RoadTripBar = () => (
+    <div>
+    <div className='label left' >Where
+              <input type="text" placeholder="Location" className="search-left" />
+            </div>
+              <div className="vl"></div>
+  
+              <div className='label' >From
+                <select className="dropdown">
+                  <option value="date">Mo 12.2. </option>
+                  <option value="date">Tu 13.2.</option>
+                  <option value="date">We 14.2.</option>
+                  <option value="date">Thr 15.2. </option>
+                </select>
+              </div>
+              <div className="vl"></div>
+  
+              <div className='label' >To
+              <select className="dropdown">
+                <option value="date">Thr 15.2. </option>
+                <option value="date">Tu 13.2.</option>
+                <option value="date">We 14.2.</option>
+                <option value="date">Thr 15.2. </option>
+              </select>
+            </div>
+              <div className="vl"></div>
+  
+              <div className='label' >Radius
+                <input type="text" placeholder="Radius" className="search-left" />
+              </div>
+                <div className="button">SEARCH</div>
+                </div>
     )
-}
 
 export default Search
